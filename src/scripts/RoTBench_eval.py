0e0ef3b0c3b187990ddc677b985b59162525dcd6
ColaZhang22 <aqzk643788315@outlook.com>
2024-11-20 10:48:13 +0900

    ADD APIBANK_eval(finished) and Toollens(Unfinished)

--- src/scripts/RoTBench_eval.py
+++ src/scripts/RoTBench_eval.py
@@ -114,14 +114,14 @@ def get_raven_action_input(action_input, test_action, config, version):
 
 def get_test_value(data):
   
-    test_value = data["label"]
+    test_value = data["predict"]
     test_action = test_value[test_value.find("Action:") + 8: test_value.find("Action Input:")]
-    if test_action[-1] == "\n":
-        test_action = test_action[:-1]
     try:
+        if test_action[-1] == "\n":
+            test_action = test_action[:-1]
         test_action_input=test_value[test_value.find("Action Input:") + 14:]
         test_action_input = json.loads(test_action_input)
-    except json.decoder.JSONDecodeError:
+    except:
         return test_action, 0
     # if isinstance(test_action_input, str):
     return test_action, test_action_input
@@ -132,23 +132,27 @@ def ts_eval(test, answer):
     tool_selection = []
     for i in range(len(answer)):
         answers = get_answer_list(answer[i])
-        test_action, test_action_input = get_test_value(test[i])
-        if not test_action_input:
+        try:
+            test_action, test_action_input = get_test_value(test[i])
+        except:
             continue
         # Check all possible answers
         right_status = 0
         for ans in answers:
-            answer_action = ans[ans.find("Action:") + 8: ans.find("Action Input:")]
-            if answer_action[-1] == "\n":
-                answer_action = answer_action[:-1]
-            if test_action == "finish":
-                test_action = answer_action
-            if not answer_action == test_action:
+            try:
+                answer_action = ans[ans.find("Action:") + 8: ans.find("Action Input:")]
+                if answer_action[-1] == "\n":
+                    answer_action = answer_action[:-1]
+                if test_action == "finish":
+                    test_action = answer_action
+                if not answer_action == test_action:
+                    continue
+                if right_status < 1:
+                    right_status = 1
+                    # print("<Tool Selection : Right>")
+                    break
+            except:
                 continue
-            if right_status < 1:
-                right_status = 1
-                # print("<Tool Selection : Right>")
-                break
         if right_status >= 1:
             tool_selection.append(i)
     a_list = []
@@ -161,31 +165,33 @@ def pi_eval(test, answer, version=0):
     global check_list
     parameter_identification = []
     for i in range(len(answer)):
-
         answers = get_answer_list(answer[i])
-        test_action, test_action_input = get_test_value(test[i])
-        if not test_action_input:
+        try:
+            test_action, test_action_input = get_test_value(test[i])
+        except:
             continue
         # Check all possible answers
         right_status = 0
         for ans in answers:
             answer_action = ans[ans.find("Action:") + 8: ans.find("Action Input:")]
-            if answer_action[-1] == "\n":
-                answer_action = answer_action[:-1]
-              
-            answer_action_input = json.loads(ans[ans.find("Action Input:") + 14:])
-            if test_action == "finish":
-                test_action = answer_action
-            if not answer_action == test_action:
+            try:
+                if answer_action[-1] == "\n":
+                    answer_action = answer_action[:-1]  
+                answer_action_input = json.loads(ans[ans.find("Action Input:") + 14:])
+                if test_action == "finish":
+                    test_action = answer_action
+                if not answer_action == test_action:
+                    continue
+                if right_status < 1:
+                    right_status = 1
+                if not answer_action_input.keys() == test_action_input.keys():
+                    continue
+                if right_status < 2:
+                    right_status = 2
+                    # print("<Parameter Identification : Right>")
+                    break
+            except:
                 continue
-            if right_status < 1:
-                right_status = 1
-            if not answer_action_input.keys() == test_action_input.keys():
-                continue
-            if right_status < 2:
-                right_status = 2
-                # print("<Parameter Identification : Right>")
-                break
         if right_status >= 2:
             parameter_identification.append(i)
     a_list = []
@@ -201,45 +207,50 @@ def cf_eval(test, answer, version=0):
         config = get_config(answer[i])
         answers = get_answer_list(answer[i])
         test_action, test_action_input = get_test_value(test[i])
-        if not test_action_input:
+        try:
+            test_action, test_action_input = get_test_value(test[i])
+        except:
             continue
         # Check all possible answers
         right_status = 0
         for ans in answers:
-            answer_action = ans[ans.find("Action:") + 8: ans.find("Action Input:")]
-            if answer_action[-1] == "\n":
-                answer_action = answer_action[:-1]
-            answer_action_input = json.loads(ans[ans.find("Action Input:") + 14:])
-            
-            
-            if test_action == "finish":
-                test_action = answer_action
-            if not answer_action == test_action:
-                continue
-            if right_status < 1:
-                right_status = 1
-            if not answer_action_input.keys() == test_action_input.keys():
-                continue
-            if right_status < 2:
-                right_status = 2
+            try:
+                answer_action = ans[ans.find("Action:") + 8: ans.find("Action Input:")]
+                if answer_action[-1] == "\n":
+                    answer_action = answer_action[:-1]
+                answer_action_input = json.loads(ans[ans.find("Action Input:") + 14:])
+                
+                
+                if test_action == "finish":
+                    test_action = answer_action
+                if not answer_action == test_action:
+                    continue
+                if right_status < 1:
+                    right_status = 1
+                if not answer_action_input.keys() == test_action_input.keys():
+                    continue
+                if right_status < 2:
+                    right_status = 2
 
-            # if answer_action == config[-1]["name"]:
-            #     answer_action = "finish"
-            # if answer_action == config[-2]["name"]:
-            #     answer_action = "ask_to_user"
-            del_key = []
-            for key, value in answer_action_input.items():
-                if value == "None":
-                    del_key.append(key)
-            for key in del_key:
-                del answer_action_input[key]
-                del test_action_input[key]
-            if not answer_action_input == test_action_input and answer_action != "finish" and answer_action != "ask_to_user":
+                # if answer_action == config[-1]["name"]:
+                #     answer_action = "finish"
+                # if answer_action == config[-2]["name"]:
+                #     answer_action = "ask_to_user"
+                del_key = []
+                for key, value in answer_action_input.items():
+                    if value == "None":
+                        del_key.append(key)
+                for key in del_key:
+                    del answer_action_input[key]
+                    del test_action_input[key]
+                if not answer_action_input == test_action_input and answer_action != "finish" and answer_action != "ask_to_user":
+                    continue
+                if right_status < 3:
+                    right_status = 3
+                    # print("<Content Filling : Right>")
+                    break
+            except:
                 continue
-            if right_status < 3:
-                right_status = 3
-                # print("<Content Filling : Right>")
-                break
         if right_status >= 3:
             content_filling.append(i)
     a_list = []
