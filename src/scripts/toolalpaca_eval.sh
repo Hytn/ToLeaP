05f65bcbcaf83e10c9af9d95c75d9b2753ce9f85
Leonard907 <ou_litu@163.com>
2024-11-26 13:39:39 +0000

    update

--- src/scripts/toolalpaca_eval.sh
+++ src/scripts/toolalpaca_eval.sh
@@ -1,12 +1,15 @@
 # Arguments
-# $1: the input data path
-# $2: (optional) the path to the data file
+# $1: sharegpt data path
+# $2: yaml config for llama factory
+# $3: model name or path
+echo "Input data path: $1"
+echo "YAML config path: $2" 
+echo "Model name/path: $3"
 
 cp $1 llamafactory_data/toolalpaca.json
 
 # Run evaluation with provided arguments
-if [ -z "$2" ]; then
-    python toolalpaca_eval.py --eval_file "$1"
-else
-    python toolalpaca_eval.py --eval_file "$1" --data_file "$2"
-fi
+python toolalpaca_eval.py \
+    --config $2 \
+    --input_file $1 \
+    --model_name $3
